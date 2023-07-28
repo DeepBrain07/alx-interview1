696ee4314bebf2d49e4e2a4c4a65dfef0e59c023
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-07-28 04:17:37 +0100

    Completed

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+def pascal_triangle(n):
+    # check if n <= 0
+    if n <= 0:
+        return []
+
+    LST = []
+    lst = [0, 1, 0]
+    LST.append([1])
+    for _ in range(n - 1):
+        new_lst = [0]
+        for j in range(len(lst)):
+            if j == len(lst) - 1:
+                new_lst.append(lst[j])
+                lst = new_lst
+                LST.append(new_lst[1:-1])
+            else:
+                new_lst.append(lst[j] + lst[j+1])
+    return LST
