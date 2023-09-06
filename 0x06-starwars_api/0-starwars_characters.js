eae897edd04984aaefc1ea7f5c80c399248c7fc1
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-09-06 01:36:19 +0100

    Updating

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,46 @@
-#!/usr/bin/node
+// #!/usr/bin/node
 
-const request = require('request');
+// const request = require('request');
+
+// const arg = process.argv[2];
+// const url = 'https://swapi-api.alx-tools.com/api/films/' + arg;
+// request(url, (err, res, body) => {
+//   if (!err && res.statusCode === 200) {
+//     const data = JSON.parse(body);
+//     const charactersLink = data.characters;
+//     charactersLink.forEach(element => {
+//       request(element, (error, response, body_) => {
+//         if (!error && response.statusCode === 200) {
+//           const data_ = JSON.parse(body_);
+//           console.log(data_.name);
+//         }
+//       });
+//     });
+//   } else {
+//     console.log('An Error Occurred!');
+//   }
+// });
+
+const request = require('request-promise');
 
 const arg = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + arg;
-request(url, (err, res, body) => {
-  if (!err && res.statusCode === 200) {
-    const data = JSON.parse(body);
-    const charactersLink = data.characters;
-    charactersLink.forEach(element => {
-      request(element, (error, response, body_) => {
-        if (!error && response.statusCode === 200) {
-          const data_ = JSON.parse(body_);
-          console.log(data_.name);
-        }
-      });
-    });
+
+const getCharacters = async (url) => {
+  const response = await request(url);
+  if (response) {
+    const charactersLink = JSON.parse(response).characters;
+    for (const link of charactersLink) {
+      const name = await request(link);
+      if (name) {
+        console.log(JSON.parse(name).name);
+      } else {
+        console.log('An Error Occured!');
+      }
+    }
   } else {
-    console.log('An Error Occurred!');
+    console.log('An Error Occured!');
   }
-});
+};
+
+getCharacters(url);
